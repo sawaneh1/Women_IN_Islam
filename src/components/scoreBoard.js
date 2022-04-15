e71beca8e047605a5901aca4570abfc47bfd150f
sawaneh <sawaneh@pop-os.localdomain>
2022-04-15 14:52:17 +0000

    added user avatar

--- src/components/scoreBoard.js
+++ src/components/scoreBoard.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./scoreboard.css";
-import logo from "../assests/test1.svg";
+import logo from "../assests/new.jpg";
 
 const ScoreBoard = () => {
   return (
