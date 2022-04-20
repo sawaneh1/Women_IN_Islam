0384221da452cca241bffe9901ba172993e32ede
sawaneh <sawaneh@pop-os.localdomain>
2022-04-20 23:09:27 +0000

    shape divider

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="app">
       <div className="divider">
         <Navbar />
-        <Home />
+        {/* <Home /> */}
         <About />
       </div>
       {/* <h1>test</h1> */}
